163f03644746cfaf868a7517436c5548b408aa3d
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-23 13:03:41 -0500

    build tests and work on fixing thunderclient

--- __tests__/basic-auth.test.js
+++ __tests__/basic-auth.test.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+const base64 = require('base-64');
+const { userModel } = require('../src/auth/models');
+
+//import function to be tested
+
 // const basicAuth = require('../src/auth/middleware/basic');
 const { sequelizeDb, UsersModel } = require('../src/auth/models/user');
 
@@ -24,16 +30,24 @@ afterAll (async () => {
 
 describe('Auth routes', (() => {
   test('allow for user signup', async () => {
-
+    const response = await request.post('/signup').send ({
+      username: '',
+      password: '',
+    });
   });
 
 
   test('allow for user signin', async () => {
-    const response = await request.post('singin').set('Authorization', 'Basic ');
+    const response = await request.post('/signin').set('Authorization', 'Basic ');
 
     expect(response.status).toEqual(200);
     expect(response.body.username).toEqual('');
   });
 
 
+
+
+
+
+  
 }));
