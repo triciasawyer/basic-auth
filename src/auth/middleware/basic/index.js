183631ab45f5ad816e6e4ee2fac79b36be80c5a5
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-24 16:29:46 -0500

    remove comments to clean up code

--- src/auth/middleware/basic/index.js
+++ src/auth/middleware/basic/index.js
@@ -6,7 +6,6 @@ const { userModel } = require('../../models');
 
 
 module.exports = async (req, res, next) => {
-  console.log('XXXXXXXXX',req.headers.authorization);
   let basicHeaderParts = req.headers.authorization.split(' ');
   let encodedString = basicHeaderParts.pop();
   let decodedString = base64.decode(encodedString);
